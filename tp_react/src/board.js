b24c989fc76ff68012cdd573fdefaf3d516c605a
quentin <quentin>
2018-11-30 23:44:47 +0100

    gestion affichage loading

--- tp_react/src/board.js
+++ tp_react/src/board.js
@@ -9,32 +9,57 @@ class Board extends Component {
     super(props);
     this.state = {
       data : [],
+      championsName:[],
       loading:true,
     };
   }
   render() {
     return (
       <section className="card-columns ">
-          {}
+          {this.display()}
       </section>
     );
   }
 
   //https://raw.githubusercontent.com/caro3801/vuejs-tpbase/master/public/champions.json
   
+  display(){
+    if(this.state.loading){
+      return(this.displayLoading);
+    }else{
+      return(this.displayLoaded());
+    }
+  }
+
+  displayLoading(){
+    return(
+      <div className="row">
+        <div className="col-md-4 col-md-offset-5"></div>
+          <div className="col-md-4 col-md-offset-5">
+            <img className="" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"/>
+          </div>
+        <div className="col-md-4 col-md-offset-5"></div>
+      </div>
+    );
+  }
+
   componentDidMount() {
     const url = 'https://raw.githubusercontent.com/caro3801/vuejs-tpbase/master/public/champions.json';
     axios.get(url)
       .then(response => {
         this.setState({
-          data: response.data,
+          data: response.data.data,
           loading: false
         });
-        console.log(response.data);
+        //console.log(response.data.data);
       })
       .catch(error => {
         console.log(error);
       });
+  }
+
+  displayLoaded = () => {
+    return "erllo"
   }
 
   //THIS CODE IS WORKING :)
