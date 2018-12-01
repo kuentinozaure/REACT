75e16b6e38d7de7c04e56a60a6d8b133d7604980
quentin <quentin>
2018-12-01 22:03:22 +0100

    generation des carte jumelle au premierer

--- tp_react/src/footer.js
+++ tp_react/src/footer.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class footer extends Component {
   render() {
     return (
-      <p>Copyright &copy; Web2 L3 UT2J</p>
+      <div className="bg-dark">
+        <nav className="navbar bg-dark">
+            <img className="img-responsive" src="https://cdn.icon-icons.com/icons2/851/PNG/128/pikachu_icon-icons.com_67535.png" />  
+            <span className="navbar-brand text-secondary"><p>Copyright &copy; Web2 L3 UT2J</p></span>
+        </nav>
+      </div>
     );
   }
 }
